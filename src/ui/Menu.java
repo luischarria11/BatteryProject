55e45b8f4487f2cbc94b67c220ee91e9970b2e63
luischarria11 <1108559191@u.icesi.edu.co>
2022-04-29 12:55:47 -0500

    solution error

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -4,83 +4,84 @@ import java.util.Scanner;
 import model.Company;
 
 public class Menu {
-	
+
 	private Company company;
 	private Scanner sc;
-	
-	public Menu(){
-        InitModel init = new InitModel();
-        company = new Company(init.createBatteries());
-		sc= new Scanner(System.in);
 
-    }
+	public Menu() {
+		InitModel init = new InitModel();
+		company = new Company(init.createBatteries());
+		sc = new Scanner(System.in);
+
+	}
 
 	public static void main(String[] args) {
 		Menu m = new Menu();
 
 		int option = 0;
-		do{
-			option= m.showMenu();
+		do {
+			option = m.showMenu();
 			m.executeOperation(option);
-			
-		}while (option!=0);
+
+		} while (option != 0);
 
 	}
 
 	public int showMenu() {
-		int option=0;
+		int option = 0;
 
 		System.out.println(
 				"Seleccione una opción para empezar\n" +
-				"(1) Para Registrar una bateria\n" +
-                "(2) Para Registrar una bateria recargable\n" +
-				"(3) Para Mostrar el total de baterias tradicionales y recargables\n"+
-				"(4) Para Calcular el promedio de costo de vida util de las baterias recargables\n"+
-				"(5) Para mostrar la informacion de todas las baterias\n" +  
-				"(0) Para salir"
-				);
-		option= sc.nextInt();
+						"(1) Para Registrar una bateria\n" +
+						"(2) Para Registrar una bateria recargable\n" +
+						"(3) Para Mostrar el total de baterias tradicionales y recargables\n" +
+						"(4) Para Calcular el promedio de costo de vida util de las baterias recargables\n" +
+						"(5) Para mostrar la informacion de todas las baterias\n" +
+						"(0) Para salir");
+		option = sc.nextInt();
 		sc.nextLine();
 		return option;
 	}
 
 	public void executeOperation(int operation) {
-		
-		switch(operation) {
-		case 0:
-			System.out.println("Bye!");
-			break;
-		case 1:
-		    registerBattery();	
-			break;
-		case 2:
-            registerRechargeableBattery();
-			break;
-	
-		case 3:
-			//System.out.println(company.showTotalBatteries());
-			//"(3) Para Mostrar el total de baterias tradicionales y recargables\n"+ 
-			break;
 
-		case 4:
-			//System.out.println("El costo promedio de vida util de las baterias recargables es: "+ company.calculateUsefulPromLifeCost());
-		    // "(4) Para Calcular el promedio de costo de vida util de las baterias recargables\n"+ 
-			break;
+		switch (operation) {
+			case 0:
+				System.out.println("Bye!");
+				break;
+			case 1:
+				registerBattery();
+				break;
+			case 2:
+				registerRechargeableBattery();
+				break;
 
-		case 5:
-			//System.out.println(company.showBatteriesInfo());
-			// "(5) Para mostrar la informacion de todas las baterias\n" +
-			break;
-		
-		default:
-			System.out.println("Error, opción no válida");
-		
+			case 3:
+				// System.out.println(company.showTotalBatteries());
+				// "(3) Para Mostrar el total de baterias tradicionales y recargables\n"+
+				break;
+
+			case 4:
+				// System.out.println("El costo promedio de vida util de las baterias
+				// recargables es: "+ company.calculateUsefulPromLifeCost());
+				// "(4) Para Calcular el promedio de costo de vida util de las baterias
+				// recargables\n"+
+				break;
+
+			case 5:
+				// System.out.println(company.showBatteriesInfo());
+				// "(5) Para mostrar la informacion de todas las baterias\n" +
+				break;
+
+			default:
+				System.out.println("Error, opción no válida");
+
 		}
 	}
 
-    public void registerBattery() {
+	public void registerBattery() {
 
-        System.out.println("Ingrese el nombre de la bateria");
+		System.out.println("Ingrese el nombre de la bateria");
 		String name;
 		name = sc.nextLine();
 
@@ -93,14 +94,14 @@ public class Menu {
 
 		System.out.println("ingrese la capacidad de la bateria");
 		voltage = sc.nextDouble();
-        
-        company.registerBattery(name, voltage, cost, capacity);
-        
-    }
 
-    public void registerRechargeableBattery() {
+		company.registerBattery(name, voltage, cost, capacity);
 
-        System.out.println("Ingrese el nombre de la bateria");
+	}
+
+	public void registerRechargeableBattery() {
+
+		System.out.println("Ingrese el nombre de la bateria");
 		String name;
 		name = sc.nextLine();
 
@@ -113,17 +114,17 @@ public class Menu {
 
 		System.out.println("ingrese la capacidad de la bateria");
 		voltage = sc.nextDouble();
-        
-        int chargerNumber; 
-        System.out.println("Ingrese el numero de cargas");
-        chargerNumber = sc.nextInt();
 
-        char type;
-        System.out.println("�cual es el tipo de bateria? Litio (l) o  Niquel-Cadio (n)");
-        type = sc.next().toCharArray()[0];
-        
-        company.registerRechargeableBattery(name, voltage, cost, capacity, chargerNumber, type);
-        
-    }
+		int chargerNumber;
+		System.out.println("Ingrese el numero de cargas");
+		chargerNumber = sc.nextInt();
+
+		char type;
+		System.out.println("cual es el tipo de bateria? Litio (l) o  Niquel-Cadio (n)?");
+		type = sc.next().toCharArray()[0];
+
+		company.registerRechargeableBattery(name, voltage, cost, capacity, chargerNumber, type);
+
+	}
 
 }
